a1e134d4c50662a4254cd47a8bc418aeb1c9264c
Your Name01echo <xitamarfx@gmail.com>
2023-01-08 15:03:47 +0200

    1.2

---
+++ workout2/againe.js
@@ -0,0 +1,37 @@
+/*--Delete the wrong answers--
+1. Which of the following is/are strings?
+a) 4
+b) 4.0
+c) '4'
+2. Which of the following is/are numbers?
+a) 4
+b) 4.0
+c) '4'
+d) -4
+3. Which of the following is/are booleans?
+a) true
+b) false
+c) "true"
+4. What is the result of 80 + 71.2?
+a) 151.2
+b) 151
+c) 8071.2
+5. What is the result of "80" + 71.2?
+a) 151.2
+b) 151
+c) "8071.2"
+6. Does 100 + 30 produce a number or a string?
+a) number
+b)string
+7. Does "100" + 30 produce a number or a string?
+a)number
+b)string
+*/
+
+// 1) C
+// 2) A b c
+// 3) all
+// 4) a
+// 5) c
+// 6) a
+// 7) b

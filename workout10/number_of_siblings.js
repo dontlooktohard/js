d3ff49efbaff04806aa4f69a12ab0ee71fb939ae
Your Name01echo <xitamarfx@gmail.com>
2023-01-10 01:41:19 +0200

    3.4

---
+++ workout10/number_of_siblings.js
@@ -0,0 +1,18 @@
+
+let numSiblings = prompt("'How many siblings do you have?");
+
+if (numSiblings !==null)  {
+    
+  if (numSiblings === 1) {
+    prompt('More than 1 sibling' )
+    
+  }
+   else if (numSiblings === 0){  
+    prompt('No siblings' )
+} 
+else  (numSiblings > 1) ;{  
+    prompt('number of siblings')
+} 
+
+}
+numSiblings();
---
+++ workout9/expressions.js
@@ -0,0 +1,12 @@
+const percentageOfWorld1 = (a) => {
+    const  population = 7900000000;
+    const  countries =[
+        'china'=(1441-'million'),
+        'spain'=(47-'million'),
+        'brazil'=(214-'million')]
+    const country = countries[countriesIndex];
+    const  percentage = country/7900000000 * 100;
+    console.log('${country} is ${percentage} of the world population.');
+
+    
+}

d62e582eea36d7bcc30b40611a27c43f560bc18c
Your Name01echo <xitamarfx@gmail.com>
2023-01-10 11:47:14 +0200

    5.2

---
+++ workout16/langauges.js
@@ -0,0 +1,22 @@
+function languageIndicator(language){
+    switch(language){
+        case 1: 
+        console.log("MOST number of native speakers!")
+        break;
+        case 2: 
+        console.log('2nd place in a number of native speakers')
+        break;
+        case 3: 
+        console.log('3rd place')
+        break;
+        case 4: 
+        console.log('Number 4')
+        break;
+        case 5: 
+        console.log('5th most spoken language')
+        break;
+
+        default:
+            console.log('not in top 5')
+    }
+}

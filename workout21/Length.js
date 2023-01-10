be13b01bbc9ab2be45b3748485cbf154b0d3c3db
Your Name01echo <xitamarfx@gmail.com>
2023-01-10 23:17:08 +0200

    6.3

---
+++ workout21/Length.js
@@ -0,0 +1,8 @@
+function stringLengths(arr) {
+    return arr.map(function(s) {
+      return s.length;
+    });
+  }
+
+  let arr = ["boo", "doooo", "hoo","ro"];
+  console.log(stringLengths(arr));

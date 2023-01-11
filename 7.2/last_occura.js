7deb35ce221fa55d39a504672a37b67cb0272230
Your Name01echo <xitamarfx@gmail.com>
2023-01-11 15:09:12 +0200

    7.2

---
+++ 7.2/last_occura.js
@@ -0,0 +1,7 @@
+// const string = "The more you know, the more you know that you don't know";
+// console.log(string.slice(42,45));
+
+
+const string1 = "The more you know, the more you know that you don't know";
+const index = string1.lastIndexOf("know");
+console.log(index);

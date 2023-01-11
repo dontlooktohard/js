03ead7f636195a1e7e0fa0883d05ab6e1b56f15e
Your Name01echo <xitamarfx@gmail.com>
2023-01-11 11:06:32 +0200

    7.1

---
+++ 7.1/remove.js
@@ -0,0 +1,13 @@
+// const getString(arr) => arr.map(Element) = Element.indexOf([3,4,4,3,6,3]){
+//     return Element
+// }
+// const noDuplicates = (arr) =>
+//     arr.filter((el, index) => arr.indexOf(el) === index);
+
+// console.log(noDuplicates([3, 4, 4, 3, 6, 3]));
+
+
+const noDuplicates = (numbers) =>
+ numbers.filter((val,index) => numbers.indexOf(val) === index);
+
+console.log(noDuplicates([3, 4, 4, 3, 6, 3]));
